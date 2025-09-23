d50e9a03ce1d406966b3c916f6b3d660a2d93e6b
Claes Adamsson <claes.adamsson@gmail.com>
2025-09-23 21:50:05 +0200

    Refactor Choreo lexer to use module structure

--- _plugins/choreo_lexer.rb
+++ _plugins/choreo_lexer.rb
@@ -1,73 +1,73 @@
 require 'rouge'
 
-class Choreo < Rouge::RegexLexer
-  title "Choreo"
-  desc "A DSL for behavior-driven testing (github.com/cladam/choreo)"
-  tag 'choreo'
-  filenames '*.chor'
-  mimetypes 'text/x-choreo'
+module Rouge
+  module Lexers
+    class Choreo < Rouge::RegexLexer
+      title "Choreo"
+      desc "A DSL for behavior-driven testing (github.com/cladam/choreo)"
+      tag 'choreo'
+      filenames '*.chor'
+      mimetypes 'text/x-choreo'
 
-  # Keywords for blocks and declarations
-  KEYWORD_DECLARATION = %w(
-    feature actors settings background scenario after test var
-  ).freeze
+      # Keywords for blocks and declarations
+      KEYWORD_DECLARATION = %w(
+        feature actors settings background scenario after test var
+      ).freeze
 
-  # Keywords for test steps
-  KEYWORD_STEP = %w(
-    given when then
-  ).freeze
+      # Keywords for test steps
+      KEYWORD_STEP = %w(
+        given when then
+      ).freeze
 
-  # Built-in actors and literal values
-  BUILTIN_LITERAL = %w(
-    Web Terminal FileSystem true false
-  ).freeze
+      # Built-in actors and literal values
+      BUILTIN_LITERAL = %w(
+        Web Terminal FileSystem true false
+      ).freeze
 
-  # All commands AND assertions, styled consistently as functions
-  COMMANDS_AND_ASSERTIONS = %w(
-    wait set_header set_cookie http_get http_post clear_header clear_cookie
-    run type wait_for_text create_file delete_file append_to_file
-    response_status_is response_time_is_below response_header_is response_body_is
-    response_body_contains expect_exit_code stdout_contains stderr_contains
-    stdout_not_contains stderr_not_contains file_exists file_not_exists
-    file_contains file_not_contains is_success
-  ).freeze
+      # All commands AND assertions, styled consistently as functions
+      COMMANDS_AND_ASSERTIONS = %w(
+        wait set_header set_cookie http_get http_post clear_header clear_cookie
+        run type wait_for_text create_file delete_file append_to_file
+        response_status_is response_time_is_below response_header_is response_body_is
+        response_body_contains expect_exit_code stdout_contains stderr_contains
+        stdout_not_contains stderr_not_contains file_exists file_not_exists
+        file_contains file_not_contains is_success
+      ).freeze
 
-  state :root do
-    # Comments
-    rule %r/#.*$/, Comment::Single
+      state :root do
+        # Comments
+        rule %r/#.*$/, Comment::Single
 
-    # Punctuation and operators
-    rule %r/[{}=:]|>=/, Punctuation
+        # Punctuation and operators
+        rule %r/[{}=:]|>=/, Punctuation
 
-    # Use the arrays defined above to find and tokenise keywords
-    rule %r/\b(?:feature|actors|settings|background|scenario|after|test|var)\b/, Keyword::Declaration
-    rule %r/\b(?:given|when|then)\b/, Keyword
-    rule %r/\b(?:Web|Terminal|FileSystem|true|false)\b/, Name::Builtin
-    rule %r/\b(#{COMMANDS_AND_ASSERTIONS.join('|')})\b/, Name::Function
+        # Use the arrays defined above to find and tokenise keywords
+        rule %r/\b(?:feature|actors|settings|background|scenario|after|test|var)\b/, Keyword::Declaration
+        rule %r/\b(?:given|when|then)\b/, Keyword
+        rule %r/\b(?:Web|Terminal|FileSystem|true|false)\b/, Name::Builtin
+        rule %r/\b(#{COMMANDS_AND_ASSERTIONS.join('|')})\b/, Name::Function
 
+        # Variable usage
+        rule %r/\$\{.*?}/, Name::Variable
 
-    # Variable usage
-    rule %r/\$\{.*?}/, Name::Variable
+        # Strings in double quotes
+        rule %r/"/, Str::Double, :string
 
-    # Strings in double quotes
-    rule %r/"/, Str::Double, :string
+        # Numbers and time values
+        rule %r/\b\d+s?\b/, Num
 
-    # Numbers and time values
-    rule %r/\b\d+s?\b/, Num
+        # Whitespace
+        rule %r/\s+/, Text::Whitespace
 
-    # Whitespace
-    rule %r/\s+/, Text::Whitespace
+        # Any other text
+        rule %r/[a-zA-Z_][a-zA-Z0-9_]*/, Text
+      end
 
-    # Any other text
-    rule %r/[a-zA-Z_][a-zA-Z0-9_]*/, Text
-  end
-
-  state :string do
-    rule %r/"/, Str::Double, :pop!
-    rule %r/\$\{.*?}/, Name::Variable # Interpolation
-    rule %r/[^"]+/, Str::Double
+      state :string do
+        rule %r/"/, Str::Double, :pop!
+        rule %r/\$\{.*?}/, Name::Variable
+        rule %r/[^"]+/, Str::Double
+      end
+    end
   end
 end
-
-# Register the lexer with Rouge
-Rouge::Lexer.register(Choreo)
