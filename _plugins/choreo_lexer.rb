251b1b17516d8fad22002e69fb99dd3cb8b2da7b
Claes Adamsson <claes.adamsson@gmail.com>
2025-09-23 17:37:52 +0200

    Refactor lexer rules and remove unused constants

--- _plugins/choreo_lexer.rb
+++ _plugins/choreo_lexer.rb
@@ -9,60 +9,36 @@ class Choreo < Rouge::RegexLexer
   filenames '*.chor'
   mimetypes 'text/x-choreo'
 
-  # --- Word Lists ---
-  KEYWORD_DECLARATION = %w(
-    feature actors settings background scenario after test var
-  ).freeze
-
-  KEYWORD_STEP = %w(
-    given when then
-  ).freeze
-
-  BUILTIN_LITERAL = %w(
-    Web Terminal FileSystem true false
-  ).freeze
-
-  COMMANDS_AND_ASSERTIONS = %w(
-    wait set_header set_cookie http_get http_post clear_header clear_cookie
-    run type wait_for_text create_file delete_file append_to_file
-    response_status_is response_time_is_below response_header_is response_body_is
-    response_body_contains expect_exit_code stdout_contains stderr_contains
-    stdout_not_contains stderr_not_contains file_exists file_not_exists
-    file_contains file_not_contains
-  ).freeze
-
-  # --- Lexer States ---
   state :root do
     rule %r/\s+/, Text::Whitespace
     rule %r/#.*$/, Comment::Single
 
-    # Keywords that are followed by other text on the same line
-    rule %r/^(feature|scenario|test|var)\b/, Keyword::Declaration, :title_line
+    # Keywords that start a block or declaration
+    rule %r/^(feature|actors|settings|background|scenario|after|var|test)\b/, Keyword::Declaration
 
-    # Standalone keywords
-    rule %r/\b(#{KEYWORD_DECLARATION.join('|')})\b/, Keyword::Declaration
-    rule %r/\b(#{KEYWORD_STEP.join('|')})\b:/, Keyword # Look for the colon
-    rule %r/\b(#{BUILTIN_LITERAL.join('|')})\b/, Name::Builtin
-    rule %r/\b(#{COMMANDS_AND_ASSERTIONS.join('|')})\b/, Name::Function
+    # Step keywords with a colon
+    rule %r/\b(given|when|then)\b:/, Keyword
+
+    # Built-in actors and literal values
+    rule %r/\b(Web|Terminal|FileSystem|true|false)\b/, Name::Builtin
+    
+    # All commands and assertions
+    rule %r/\b(wait|set_header|set_cookie|http_get|http_post|clear_header|clear_cookie|run|type|wait_for_text|create_file|delete_file|append_to_file|response_status_is|response_time_is_below|response_header_is|response_body_is|response_body_contains|expect_exit_code|stdout_contains|stderr_contains|stdout_not_contains|stderr_not_contains|file_exists|file_not_exists|file_contains|file_not_contains)\b/, Name::Function
 
     # Punctuation and operators
     rule %r/[{}=:]|>=/, Punctuation
 
-    # Strings in double quotes
+    # Strings, which can contain variables
     rule %r/"/, Str::Double, :string
 
     # Numbers and time values
     rule %r/\b\d+s?\b/, Num
+    
+    # Any other text that isn't a keyword but might be a variable name or other identifier
+    rule %r/[a-zA-Z_][a-zA-Z0-9_]*/, Text
   end
 
-  # This state handles the rest of a line after a keyword like 'feature'
-  state :title_line do
-    rule %r/"/, Str::Double, :string  # Capture strings in the title
-    rule %r/\n/, Text::Whitespace, :pop! # Exit state on newline
-    rule %r/[^"\n]+/, Text # Everything else is plain text
-  end
-
-  # This state handles content inside strings
+  # State for handling content inside strings
   state :string do
     rule %r/"/, Str::Double, :pop!
     rule %r/\$\{.*?}/, Name::Variable # Interpolation
