2e358d4d44fb09a982b9b0bf16ae5cf68a40b267
Claes Adamsson <claes.adamsson@gmail.com>
2025-09-23 19:04:35 +0200

    Refactor Choreo lexer keyword and command handling

--- _plugins/choreo_lexer.rb
+++ _plugins/choreo_lexer.rb
@@ -1,5 +1,3 @@
-# _plugins/choreo_lexer.rb
-
 require 'rouge'
 
 class Choreo < Rouge::RegexLexer
@@ -9,40 +7,63 @@ class Choreo < Rouge::RegexLexer
   filenames '*.chor'
   mimetypes 'text/x-choreo'
 
+  # Keywords for blocks and declarations
+  KEYWORD_DECLARATION = %w(
+    feature actors settings background scenario after test var
+  ).freeze
+
+  # Keywords for test steps
+  KEYWORD_STEP = %w(
+    given when then
+  ).freeze
+
+  # Built-in actors and literal values
+  BUILTIN_LITERAL = %w(
+    Web Terminal FileSystem true false
+  ).freeze
+
+  # All commands AND assertions, styled consistently as functions
+  COMMANDS_AND_ASSERTIONS = %w(
+    wait set_header set_cookie http_get http_post clear_header clear_cookie
+    run type wait_for_text create_file delete_file append_to_file
+    response_status_is response_time_is_below response_header_is response_body_is
+    response_body_contains expect_exit_code stdout_contains stderr_contains
+    stdout_not_contains stderr_not_contains file_exists file_not_exists
+    file_contains file_not_contains is_success
+  ).freeze
+
   state :root do
-    # Whitespace and Comments
-    rule %r/\s+/m, Text::Whitespace
+    # Comments
     rule %r/#.*$/, Comment::Single
-
-    # Keywords that start a block or declaration
-    rule %r/^(feature|actors|settings|background|scenario|after|var|test)\b/, Keyword::Declaration
-
-    # Step keywords with a colon
-    rule %r/^\s*(given|when|then):/m, Keyword
-
-    # Built-in actors and literal values
-    rule %r/\b(Web|Terminal|FileSystem|true|false)\b/, Name::Builtin
-    
-    # All commands and assertions
-    rule %r/\b(wait|set_header|set_cookie|http_get|http_post|clear_header|clear_cookie|run|type|wait_for_text|create_file|delete_file|append_to_file|response_status_is|response_time_is_below|response_header_is|response_body_is|response_body_contains|expect_exit_code|stdout_contains|stderr_contains|stdout_not_contains|stderr_not_contains|file_exists|file_not_exists|file_contains|file_not_contains)\b/, Name::Function
 
     # Punctuation and operators
     rule %r/[{}=:]|>=/, Punctuation
 
-    # Strings, which can contain variables
+    # Use the arrays defined above to find and tokenise keywords
+    rule %r/\b(#{KEYWORD_DECLARATION.join('|')})\b/, Keyword::Declaration
+    rule %r/\b(#{KEYWORD_STEP.join('|')})\b/, Keyword
+    rule %r/\b(#{BUILTIN_LITERAL.join('|')})\b/, Name::Builtin
+    rule %r/\b(#{COMMANDS_AND_ASSERTIONS.join('|')})\b/, Name::Function
+
+    # Variable usage
+    rule %r/\$\{.*?}/, Name::Variable
+
+    # Strings in double quotes
     rule %r/"/, Str::Double, :string
 
     # Numbers and time values
     rule %r/\b\d+s?\b/, Num
-    
-    # Any other text (variable names, test names etc.)
+
+    # Whitespace
+    rule %r/\s+/, Text::Whitespace
+
+    # Any other text
     rule %r/[a-zA-Z_][a-zA-Z0-9_]*/, Text
   end
 
-  # State for handling content inside strings
   state :string do
     rule %r/"/, Str::Double, :pop!
-    rule %r/\$\{.*?}/, Name::Variable
+    rule %r/\$\{.*?}/, Name::Variable # Interpolation
     rule %r/[^"]+/, Str::Double
   end
 end
